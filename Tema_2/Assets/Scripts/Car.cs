aa64983fe0b9f5a7ac9ea03998731f2ad0ef186e
Jbernat88 <91455587+Jbernat88@users.noreply.github.com>
2021-10-11 11:10:29 +0200

    script por consola

---
+++ Tema_2/Assets/Scripts/Car.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    // Variable que guarda el sonido del vehiculo
+   [SerializeField] private string sound = "¡Brum, brum";
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tema_2/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    /*
+    public int playerAge = 35;
+    private float playerSpeed = 5.25f;
+    public string playerName = "Joan";
+   [SerializeField] private bool game0ver;
+    */
+
+    public int x = 5;
+    public int y = 2;
+
+    public float a = 5f;
+    public float b = 2f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"Suma: {x} + {y} = {x + y}");
+        Debug.Log("Resta: " + x + "-" + y + " = " + (x - y));  
+        Debug.Log($"Division: {x} + {y} ={x / y}");
+        Debug.Log($"Multiplicacion: {x} + {y} = {x * y}");
+      
+        
+
+
+
+
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
