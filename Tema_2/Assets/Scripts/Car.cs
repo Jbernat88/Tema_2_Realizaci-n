34c65ad4b4280ead0370ad7dd33f4fe1bcf2da87
Jbernat88 <91455587+Jbernat88@users.noreply.github.com>
2021-10-19 08:23:11 +0200

    coche golfo

--- Tema_2/Assets/Scripts/Car.cs
+++ Tema_2/Assets/Scripts/Car.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Car : MonoBehaviour
 {
     // Variable que guarda el sonido del vehiculo
-   [SerializeField] private string sound = "¡Brum, brum";
+    [SerializeField] private string sound = "¡Brum, brum";
 
+    [SerializeField] private string name; //Variable que designa el nombre de los vehiculos
+    [SerializeField] private int numberWheels = 4; // Variable que guarda el numero de ruedas
+    public bool hasSiren = false;
+    [SerializeField] private bool isOn = true;
 
 
 
@@ -16,10 +20,37 @@ public class Car : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
+    { /*
+        Debug.Log($"{name} tiene {numberWheels} ruedas y hace {sound}"); 
 
+
+        if (hasSiren)
+
+        {
+            Debug.Log($"{name} tiene sirena"); //Es un operador para idicar la variable sirena
+        */
+        if (isOn == true)
+        {
+            Debug.Log($"{sound}");
+        }
+        else if (hasSiren == true)
+
+        {
+            Debug.Log($"{name} hará {sound} cuando se ponga en marcha");
+        }
+        else
+        {
+            Debug.Log($"{name} no esta en marcha");
+        }
+
+
+
+
+
+    
+    }
+   
+    
     // Update is called once per frame
     void Update()
     {
--- Tema_2/Assets/Scripts/MyFirstScript.cs
+++ Tema_2/Assets/Scripts/MyFirstScript.cs
@@ -17,16 +17,48 @@ public class MyFirstScript : MonoBehaviour
     public float a = 5f;
     public float b = 2f;
 
+    public bool isRaining = false;
+
+    public int playerAge = 35;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        /*
         Debug.Log($"Suma: {x} + {y} = {x + y}");
         Debug.Log("Resta: " + x + "-" + y + " = " + (x - y));  
-        Debug.Log($"Division: {x} + {y} ={x / y}");
+        Debug.Log($"Division: {x} + {y} = {x / y}");
         Debug.Log($"Multiplicacion: {x} + {y} = {x * y}");
-      
+        Debug.Log(string.Format("Division: ¨{0} / {1} = {2}", x, y, x * y));
+        */
+       /*      
+        if(isRaining == true)
+        {
+            Debug.Log("Lleva paraguas");
+        } 
+        else
+        {
+            Debu.Log("No lleves paraguas");
+        }
+        */
+
+        if (playerAge >= 18)
+        {
+            Debug.Log("Eres mayor de edad");
+
+        }else if (playerAge >= 13)
+        {
+            Debug.Log("Eres adolescente");
+        }
+        else
+        {
+            Debug.Log("Eres un niño");
+        }
+         
+        
+
         
 
 
